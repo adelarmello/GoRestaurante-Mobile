bc2d547e18ac7408d464584b595d3b97f4fd8d23
Adelar <adelarmello8@gmail.com>
2020-08-26 01:05:12 -0300

    Selecionar/Deselecionar a categoria

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -92,6 +92,11 @@ const Dashboard: React.FC = () => {
 
   function handleSelectCategory(id: number): void {
     // Select / deselect category
+    if (selectedCategory === id) {
+      setSelectedCategory(undefined);
+      return;
+    }
+    setSelectedCategory(id);
   }
 
   return (
